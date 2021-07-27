48037b773a3b97ca2fb9592d7ba1d4328c813aa4
igor.shimauti <igor.shimauti@linx.com.br>
2021-07-27 12:26:01 -0300

    Projeto desenvolvido no bootcamp fullstack da DIO

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/controller/ProductController.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.experts.productcatalog.controller;
+
+import one.digitalinnovation.experts.productcatalog.model.Product;
+import one.digitalinnovation.experts.productcatalog.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    public Optional<Product> readById(@PathVariable Long productId) {
+        return productRepository.findById(productId);
+    }
+}
---
+++ shopping-cart/src/main/java/one/digitalinnovation/experts/shoppingcart/model/Cart.java
@@ -0,0 +1,37 @@
+package one.digitalinnovation.experts.shoppingcart.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.List;
+
+@RedisHash("cart")
+public class Cart {
+
+    @Id
+    private Long id;
+    private List<Item> items;
+
+    public Cart() {
+    }
+
+    public Cart(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
